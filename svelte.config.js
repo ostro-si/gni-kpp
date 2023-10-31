9a8688bb1d36aa7adad66d44960662252949f778
kjacks <kjack227@gmail.com>
2023-10-31 15:32:35 +0100

    Troubleshooting gh-pages deploy setup

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '' : ''
+			base: process.env.NODE_ENV === 'production' ? '/gni-kpp' : ''
 		},
 		prerender: {
 			handleHttpError: 'warn'
