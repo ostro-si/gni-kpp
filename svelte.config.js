56458d0e22a9cd4a9050e0d7df48e31ee5922293
kjacks <kjack227@gmail.com>
2023-10-31 14:59:03 +0100

    changed production base url

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/gni-kpp' : ''
+			base: process.env.NODE_ENV === 'production' ? '' : ''
 		},
 		prerender: {
 			handleHttpError: 'warn'
